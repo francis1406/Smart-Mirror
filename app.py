9bfc5093cdcd46cf992f0ee50a074c6c96dcc4a9
FrancisJr <francisdaluz@ucl.br>
2021-10-19 04:07:02 -0300

    Adição de hora e data realtime

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
-from datetime import datetime
-import Classes.sensor as sensorRasp
-import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/teste')
-def Sensor():
-   
-    print("Distancia: {:.2f} cm".format(sensorRasp.distancia * 100))
-    teste = sensorRasp.funcaola()
-    return render_template('index.html', hora=teste, data=teste)
 
 
 @app.route('/')
 def index():
-    url = 'http://worldtimeapi.org/api/timezone/America/Sao_Paulo'
-    resposta = requests.get(url)
 
-    while True:
-        hora = datetime.fromisoformat(resposta.json()["datetime"]).strftime("%H:%M:%S")
-        data = datetime.fromisoformat(resposta.json()["datetime"]).strftime("%d/%m/%Y")
-        return render_template('index.html', hora=hora, data=data)
-
-    # return render_template('index.html', hora=hora, data=data, my_list=[0,1,2,3,4,5])
+    return render_template('index.html', my_list=[0,1,2,3,4,5])
 
 
 if __name__ == '__main__':
